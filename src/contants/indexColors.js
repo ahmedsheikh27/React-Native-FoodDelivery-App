604198b7db78e06f2950d4ce3e5cef52fdd307c3
Muhammad Ahmed <932ahmedsheikh@gmail.com>
2022-08-20 20:58:48 +0500

    assd

--- src/contants/indexColors.js
+++ src/contants/indexColors.js
@@ -1,5 +1,6 @@
 import Colors from "./contants/Colors";
 import Images from "./Images";
 import Fonts from "./Fonts";
+import General from "./General";
 
-export {Colors, Images, Fonts}
+export {Colors, Images, Fonts, General}
