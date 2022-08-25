b81309af72679f879ca03674e47b2bb82920b81a
Muhammad Ahmed <932ahmedsheikh@gmail.com>
2022-08-25 22:50:22 +0500

    Bottom-Navigation Zere-Tamir

--- src/views/navigation/BottomNavigator.js
+++ src/views/navigation/BottomNavigator.js
@@ -2,50 +2,19 @@ import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Home from '../screens/Home';
 import OnBoardScreen from '../screens/OnBoardScreen';
+import { Icon } from 'react-native-vector-icons/MaterialIcons';
 import { COLORS } from '../../consts/Colors';
-import { Icon } from 'react-native-vector-icons/FontAwesome';
 
 const Tab = createBottomTabNavigator();
 
 const BottomNavigator = () => {
-  return ( <Tab.Navigator
-    tabBarOptions={{
-      style: {
-        height: 55,
-        borderTopWidth: 0,
-        elevation: 0,
-      },
-      showLabel: false,
-      activeTintColor: COLORS.primary,
-    }}>
-    <Tab.Screen
-      name="HomeScreen"
-      component={Home}
-      options={{
-        tabBarIcon: ({color}) => (
-          <Icon name="home-filled" color={color} size={28} />
-        ),
-      }}
-    />
-    <Tab.Screen
-      name="LocalMall"
-      component={OnBoardScreen}
-      options={{
-        tabBarIcon: ({color}) => (
-          <Icon name="local-mall" color={color} size={28} />
-        ),
-      }}
-    />
-    <Tab.Screen
-      name="Cart"
-      component={CartScreen}
-      options={{
-        tabBarIcon: ({color}) => (
-          <Icon name="shopping-cart" color={color} size={28} />
-        ),
-      }}
-    />
-  </Tab.Navigator>
-);
-};
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={Home} 
+      options={{tabBarIcon: ({color})=><Icon name ='home-filled'/>}}
+      />
+      <Tab.Screen name="Settings" component={OnBoardScreen} />
+    </Tab.Navigator>
+  );
+}
 export default BottomNavigator
